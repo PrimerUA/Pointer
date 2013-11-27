f47055bc5bcbf74192a7dd0bde267f3bea29ced3
Roman Bielokon <roman_slavyansk@mail.ru>
2013-11-27 23:23:40 +0200

    created base activity and login activity

---
+++ Pointer/src/com/primerworldapps/pointer/ui/activity/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.primerworldapps.pointer.ui.activity;
+
+import android.app.Activity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kvest
+ * Date: 27.11.13
+ * Time: 22:41
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class BaseActivity extends Activity {
+}
---
+++ Pointer/src/com/primerworldapps/pointer/ui/activity/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.primerworldapps.pointer.ui.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kvest
+ * Date: 24.11.13
+ * Time: 17:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoginActivity extends BaseActivity {
+    @Override
+    public void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+    }
+}
