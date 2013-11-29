8a741e6691f3dedbde2aef94ebfbaf9b2f7912ae
Roman Bielokon <roman_slavyansk@mail.ru>
2013-11-29 19:02:25 +0200

    added Google+ login

--- Pointer/src/com/primerworldapps/pointer/ui/activity/LoginActivity.java
+++ Pointer/src/com/primerworldapps/pointer/ui/activity/LoginActivity.java
@@ -1,14 +1,23 @@
 package com.primerworldapps.pointer.ui.activity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.plus.PlusClient;
+import com.google.android.gms.plus.model.people.Person;
 import com.primerworldapps.pointer.R;
 
 /**
@@ -18,14 +27,91 @@ import com.primerworldapps.pointer.R;
  * Time: 17:24
  * To change this template use File | Settings | File Templates.
  */
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements GooglePlayServicesClient.ConnectionCallbacks,
+                                                           GooglePlayServicesClient.OnConnectionFailedListener{
     private static final float DEFAULT_ZOOM_LEVEL = 13.5f;
+    private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
+
+    private PlusClient plusClient;
+
     @Override
     public void onCreate(Bundle savedState) {
         super.onCreate(savedState);
         setContentView(R.layout.login_activity);
 
+        plusClient = new PlusClient.Builder(this, this, this)
+                                   .setVisibleActivities("http://schemas.google.com/AddActivity", "http://schemas.google.com/BuyActivity")
+                                   .build();
+        findViewById(R.id.google_plus_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginGooglePlus();
+            }
+        });
+
         initBackgroundMap();
+    }
+
+    private void loginGooglePlus() {
+        if (!plusClient.isConnected()) {
+            plusClient.connect();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (isFinishing()) {
+            plusClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        plusClient.disconnect();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_RESOLVE_ERR && resultCode == RESULT_OK) {
+            plusClient.connect();
+        }
+        //TODO
+        //uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (result.hasResolution()) {
+            try {
+                result.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+            } catch (IntentSender.SendIntentException e) {
+                plusClient.connect();
+            }
+        } else {
+            //TODO
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        //TODO
+        //plusClient.getCurrentPerson()
+        String text = "From Google+:\nName: " + plusClient.getCurrentPerson().getName().getFormatted() + "\n";
+        text += "First Name: " + plusClient.getCurrentPerson().getName().getGivenName() + "\n";
+        text += "FLast Name: " + plusClient.getCurrentPerson().getName().getFamilyName() + "\n";
+        text += "gender:" + plusClient.getCurrentPerson().getGender() + "\n";
+        text += "email: " + plusClient.getAccountName() + "\n";
+        Log.d("KVEST_TAG", text);
+    }
+
+    @Override
+    public void onDisconnected() {
+        Log.d("KVEST_TAG", "onDisconnected");
+        //TODO
     }
 
     private void initBackgroundMap() {
