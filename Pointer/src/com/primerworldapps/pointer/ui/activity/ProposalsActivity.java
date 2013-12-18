d06d9a2a8072340d766f6d5a65fdbd1619096f60
Roman Bielokon <roman_slavyansk@mail.ru>
2013-12-18 23:02:54 +0200

    implemented proposals map fragment

--- Pointer/src/com/primerworldapps/pointer/ui/activity/ProposalsActivity.java
+++ Pointer/src/com/primerworldapps/pointer/ui/activity/ProposalsActivity.java
@@ -2,9 +2,11 @@ package com.primerworldapps.pointer.ui.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.primerworldapps.pointer.R;
 import com.primerworldapps.pointer.ui.fragment.ProposalsListFragment;
+import com.primerworldapps.pointer.ui.fragment.ProposalsMapFragment;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,7 +15,8 @@ import com.primerworldapps.pointer.ui.fragment.ProposalsListFragment;
  * Time: 21:42
  * To change this template use File | Settings | File Templates.
  */
-public class ProposalsActivity extends BaseActivity {
+public class ProposalsActivity extends BaseActivity implements ProposalsMapFragment.OnProposalSelect,
+                                                               ProposalsListFragment.OnProposalSelect {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +25,17 @@ public class ProposalsActivity extends BaseActivity {
         if (savedInstanceState == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             try {
-                transaction.add(R.id.fragment_container, new ProposalsListFragment());
+                transaction.add(R.id.fragment_container, new ProposalsMapFragment());
             } finally {
                 transaction.commit();
             }
         } else {
             //TODO
         }
+    }
+
+    @Override
+    public void onProposalSelected(long proposalId) {
+        Log.d("KVEST_TAG", "selected proposal id=" + proposalId);
     }
 }
